8c89433814ad5d0dea1660eca7686859c2111091
Phanuphong <phanuphongkuharungkitcharoen@gmail.com>
2026-01-21 14:47:24 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,31 @@
 // Write your code below
+
+const consoleInput = process.argv[2]
+const inputNumber = Number(consoleInput)
+
+for (let i = 1; i <= inputNumber; i++) {
+    if(Number.isNaN(inputNumber)){
+      
+    }
+    else if (inputNumber < 0 ){
+      
+    }
+    else{
+        if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i.toString());
+      }
+    }
+}
+
+   
+   
+    
+
+
+  
